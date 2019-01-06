d39fd721dfef074b0400143eaba0201f3e658566
karantin2020 <karantin3030@mail.ru>
2019-01-06 21:31:23 +0500

    feat: Added password flag, edited request/response types
    Added password flag to cli to decrypt db info.
    Added sign and encrypt info to RegisterClientRequest. Added PubEncKey to RegisterClientResponse
    
    * Flags
    * Types

--- types.go
+++ types.go
@@ -30,6 +30,12 @@ type NewTokenResponse struct {
 // RegisterClientRequest sent to jwtis to register new client
 type RegisterClientRequest struct {
 	Kid string `json:"kid"` // Keys id to use
+
+	// Sign and encrypt keys config. If not provided then use default JWTIS values
+	SigAlg  string `json:"sig_alg"`  // algorithn to be used for sign
+	SigBits string `json:"sig_bits"` // key size in bits for sign
+	EncAlg  string `json:"enc_alg"`  // algorithn to be used for encrypt
+	EncBits string `json:"enc_bits"` // key size in bits for encrypt
 }
 
 // RegisterClientResponse sent to client after it's registration
@@ -37,4 +43,5 @@ type RegisterClientResponse struct {
 	Kid         string          `json:"kid"`          // Keys id to use
 	ClientToken string          `json:"client_token"` // Client token given after registration
 	PubSigKey   jose.JSONWebKey `json:"pub_sig_key"`  // Public sign key to verify AuthTokens
+	PubEncKey   jose.JSONWebKey `json:"pub_enc_key"`  // Public enc key to decrypt RefreshTokens
 }
